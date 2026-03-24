5e0b51041ae8478f51b7db3d7762c7810f668de6
andafnina <andafnina5@gmail.com>
2026-03-24 13:16:53 +0200

    add render port support

--- server.js
+++ server.js
@@ -226,6 +226,9 @@ app.get("/ratings/:workerName", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server started on http://localhost:3000");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
